77c696e5c507431423918ba65d22d937fb70ecec
Siyuan Ren <netheril96@gmail.com>
2024-07-27 18:16:43 +0800

    Use Session

--- steam_api.py
+++ steam_api.py
@@ -9,55 +9,54 @@ class App(TypedDict):
     name: str
 
 
-def query_all_apps() -> list[App]:
-    return requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v2").json()[
+def query_all_apps(session: requests.Session) -> list[App]:
+    return session.get("http://api.steampowered.com/ISteamApps/GetAppList/v2").json()[
         "applist"
     ]["apps"]
 
 
-def query_app_details(appid: int):
-    return requests.get(
+def query_app_details(session: requests.Session, appid: int):
+    return session.get(
         "https://store.steampowered.com/api/appdetails", params={"appids": str(appid)}
     ).json()
 
 
-def query_app_reviews(appid: int):
+def query_app_reviews(session: requests.Session, appid: int):
     cursor = "*"
 
-    with requests.Session() as session:
+    def query():
+        return session.get(
+            f"https://store.steampowered.com/appreviews/{appid}",
+            params={
+                "json": "1",
+                "filter": "recent",
+                "cursor": cursor,
+                "num_per_page": 100,
+            },
+        ).json()
 
-        def query():
-            return session.get(
-                f"https://store.steampowered.com/appreviews/{appid}",
-                params={
-                    "json": "1",
-                    "filter": "recent",
-                    "cursor": cursor,
-                    "num_per_page": 100,
-                },
-            ).json()
-
-        data = query()
-        if not data["success"]:
-            return []
-        result = [data]
-        with tqdm(total=data["query_summary"]["total_reviews"]) as t:
-            while True:
-                t.update(len(data["reviews"]))
-                data = query()
-                if not data["success"]:
-                    break
-                result.append(data)
-                if not data["reviews"]:
-                    break
-                cursor = data["cursor"]
-                if cursor == "*":
-                    break
-                time.sleep(2)
-        return result
+    data = query()
+    if not data["success"]:
+        return []
+    result = [data]
+    with tqdm(total=data["query_summary"]["total_reviews"]) as t:
+        while True:
+            t.update(len(data["reviews"]))
+            data = query()
+            if not data["success"]:
+                break
+            result.append(data)
+            if not data["reviews"]:
+                break
+            cursor = data["cursor"]
+            if cursor == "*":
+                break
+            time.sleep(2)
+    return result
 
 
 if __name__ == "__main__":
     import random
 
-    print(query_app_reviews(1041720))
+    with requests.session() as s:
+        print(query_app_reviews(s, 1041720))
