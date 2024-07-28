023823d0e0858837ff99f3c339b7dea7a9136cdc
Siyuan Ren <netheril96@gmail.com>
2024-07-28 13:28:23 +0800

    Use sqlite and zstd for storage

--- steam_fetcher.py
+++ steam_fetcher.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import time
 import requests
 import traceback
+import zstandard
+import sqlite3
 
 
 def main():
@@ -13,23 +15,52 @@ def main():
         with open("apps.json", "w") as f:
             json.dump(all_apps, f, ensure_ascii=False)
         print("Fetching all app details...")
-        for app in tqdm(all_apps):
-            if app.get("appid") and app.get("name"):
-                filename = f'app_details/{app["appid"]}.json'
-                try:
-                    with open(filename, "x") as f:
-                        details = steam_api.query_app_details(s, app["appid"])
-                        json.dump(details, f, ensure_ascii=False)
-                    time.sleep(0.93)
-                except FileExistsError:
+        compressor = zstandard.ZstdCompressor(level=9)
+        with sqlite3.connect("raw.db") as conn:
+            cursor = conn.cursor()
+            cursor.executescript(
+                """
+                    create table if not exists
+                        app_details
+                        (appid integer primary key, compressed_details blob);
+                    pragma journal_mode = wal;
+                """
+            )
+            conn.commit()
+            insert_count = 0
+            for app in tqdm(all_apps):
+                if not app.get("appid") or not app.get("name"):
                     continue
+                cursor.execute(
+                    "select count(1) from app_details where appid = ?",
+                    (app["appid"],),
+                )
+                (count,) = cursor.fetchone()
+                if count:
+                    continue  # Already saved
+                try:
+                    details = steam_api.query_app_details(s, app["appid"])
+                    data = compressor.compress(
+                        json.dumps(details, ensure_ascii=False).encode("utf-8")
+                    )
                 except KeyboardInterrupt:
-                    os.remove(filename)
-                    raise
+                    break
                 except:
                     traceback.print_exc()
-                    os.remove(filename)
-                    continue
+                    data = None
+                cursor.execute(
+                    "insert into app_details (appid, compressed_details) values (?, ?)",
+                    (app["appid"], data),
+                )
+                conn.commit()
+                insert_count += 1
+                if insert_count % 200 == 0:
+                    cursor.executescript(
+                        """
+                            pragma optimize;
+                        """
+                    )
+                time.sleep(0.93)
 
 
 if __name__ == "__main__":
